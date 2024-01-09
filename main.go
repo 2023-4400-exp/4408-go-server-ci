6d93ff0312de45189987f61478455eed2fa49dd1
oumi08 <m20025@g.metro-cit.ac.jp>
2024-01-09 15:43:37 +0900

    third

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 
 func Hello() echo.HandlerFunc {
     return func(c echo.Context) error {
-        return c.String(http.StatusOK, "学生番号: 一言 ver.2")
+        return c.String(http.StatusOK, "学生番号: this is real ver.2")
     }
 }
 
